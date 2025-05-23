9f2552a3ebca84d6db23b46c5d0b9a84f0f38e6e
UdhyaKumarKMIT <udhyak2004@gmail.com>
2025-05-23 23:14:33 +0530

    UI

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -11,19 +11,19 @@ const Header = () => {
           <h1>
           BEAUTY<span>•</span>
           </h1>
-          <p className="tagline">COSMETICS</p>
+          <p className="tagline">COSMETICS Products</p>
         </div>
 
         <nav className="main-nav">
           <ul>
             <li>
-              <a href="/shop">SHOP Products </a>
+              <a href="/shop">SHOP</a>
             </li>
             <li>
-              <a href="/bestsellers">BEST Products</a>
+              <a href="/bestsellers">BEST</a>
             </li>
             <li>
-              <a href="/skincare">SKINCARE Products</a>
+              <a href="/skincare">SKINCARE</a>
             </li>
             <li>
               <a href="/makeup">MAKEUP</a>
