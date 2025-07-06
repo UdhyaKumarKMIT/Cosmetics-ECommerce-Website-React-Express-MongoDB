3a8fb589aa00d496f74dd1c3886663652b595147
UdhyaKumarKMIT <udhyak2004@gmail.com>
2025-07-06 23:30:51 +0530

    UI2794r

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -10,9 +10,9 @@ const Footer = () => {
             <div className="navlinksBox">
               <h3>SHOP</h3>
               <ul>
-                <li><a href="#">Shop Online Now </a></li>
-                <li><a href="#">Best Health care products</a></li>
-                <li><a href="#">Natural skin care product</a></li>
+                <li><a href="#">Shop </a></li>
+                <li><a href="#">Best</a></li>
+                <li><a href="#">Natural </a></li>
                 <li><a href="#">MakeUp </a></li>
               </ul>
             </div>
