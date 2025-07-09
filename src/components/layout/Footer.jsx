a22361716a04fee11dc053280cbd50dc7d2e6510
UdhyaKumarKMIT <udhyak2004@gmail.com>
2025-07-09 23:52:07 +0530

    ui changesh

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -10,8 +10,8 @@ const Footer = () => {
             <div className="navlinksBox">
               <h3>SHOP</h3>
               <ul>
-                <li><a href="#">Shop </a></li>
-                <li><a href="#">Best </a></li>
+                <li><a href="#">Shop Online now </a></li>
+                <li><a href="#">Best selling products </a></li>
                 <li><a href="#">Natural Skincare Products </a></li>
                 <li><a href="#">MakeUp Hair Care Products</a></li>
               </ul>
