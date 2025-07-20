c51d79f5c4d6732cec897a7aae42985a110ad424
UdhyaKumarKMIT <udhyak2004@gmail.com>
2025-07-20 23:04:51 +0530

    Hellodfd343ds

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -85,15 +85,163 @@ const Header = () => {
               <a href="/makeup">MAKEUP Products  </a>
             </li> <li>
               <a href="/makeup">MAKEUP Products  </a>
-            </li> <li>
-              <a href="/makeup">MAKEUP Products  </a>
-            </li> <li>
-              <a href="/makeup">MAKEUP Products  </a>
-            </li> <li>
-              <a href="/makeup">MAKEUP Products  </a>
-            </li> <li>
-              <a href="/makeup">MAKEUP Products  </a>
-            </li>
+            </li>  int n=nums.size();
+
+if(n==1) return 0;
+
+if(nums[0]>nums[1])
+    return 0;
+
+
+if(nums[n-1]>nums[n-2])
+    return n-1;
+
+int low=1,high=n-2;
+
+while(low<=high)
+{
+    int mid=low+(high-low)/2;
+
+    if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1])
+        return mid;
+
+
+    if(nums[mid]>nums[mid-1])
+        low=mid+1;
+    else
+        high=mid-1;
+}
+return -1; int n=nums.size();
+
+if(n==1) return 0;
+
+if(nums[0]>nums[1])
+    return 0;
+
+
+if(nums[n-1]>nums[n-2])
+    return n-1;
+
+int low=1,high=n-2;
+
+while(low<=high)
+{
+    int mid=low+(high-low)/2;
+
+    if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1])
+        return mid;
+
+
+    if(nums[mid]>nums[mid-1])
+        low=mid+1;
+    else
+        high=mid-1;
+}
+return -1; int n=nums.size();
+
+if(n==1) return 0;
+
+if(nums[0]>nums[1])
+    return 0;
+
+
+if(nums[n-1]>nums[n-2])
+    return n-1;
+
+int low=1,high=n-2;
+
+while(low<=high)
+{
+    int mid=low+(high-low)/2;
+
+    if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1])
+        return mid;
+
+
+    if(nums[mid]>nums[mid-1])
+        low=mid+1;
+    else
+        high=mid-1;
+}
+return -1; int n=nums.size();
+
+if(n==1) return 0;
+
+if(nums[0]>nums[1])
+    return 0;
+
+
+if(nums[n-1]>nums[n-2])
+    return n-1;
+
+int low=1,high=n-2;
+
+while(low<=high)
+{
+    int mid=low+(high-low)/2;
+
+    if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1])
+        return mid;
+
+
+    if(nums[mid]>nums[mid-1])
+        low=mid+1;
+    else
+        high=mid-1;
+}
+return -1; int n=nums.size();
+
+if(n==1) return 0;
+
+if(nums[0]>nums[1])
+    return 0;
+
+
+if(nums[n-1]>nums[n-2])
+    return n-1;
+
+int low=1,high=n-2;
+
+while(low<=high)
+{
+    int mid=low+(high-low)/2;
+
+    if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1])
+        return mid;
+
+
+    if(nums[mid]>nums[mid-1])
+        low=mid+1;
+    else
+        high=mid-1;
+}
+return -1; int n=nums.size();
+
+if(n==1) return 0;
+
+if(nums[0]>nums[1])
+    return 0;
+
+
+if(nums[n-1]>nums[n-2])
+    return n-1;
+
+int low=1,high=n-2;
+
+while(low<=high)
+{
+    int mid=low+(high-low)/2;
+
+    if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1])
+        return mid;
+
+
+    if(nums[mid]>nums[mid-1])
+        low=mid+1;
+    else
+        high=mid-1;
+}
+return -1;
            
             <li>
   <Link to="/signup">Sign Up Page</Link>
