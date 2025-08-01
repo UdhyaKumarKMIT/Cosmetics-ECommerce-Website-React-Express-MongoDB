018aaa966e64c4da0686fb85a4f96d3136fe37d2
UdhyaKumarKMIT <udhyak2004@gmail.com>
2025-08-01 23:47:55 +0530

    Word145

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -26,6 +26,56 @@ const Header = () => {
           </ul>
         </nav>
 
+        <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+        <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+        <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+        <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav><nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+
         {/* Header Actions */}
         <div className="header-actions">
           <button className="icon-button">
