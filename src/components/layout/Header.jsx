031e457b4ff5adacc599b378b90564448e861972
UdhyaKumarKMIT <udhyak2004@gmail.com>
2025-08-10 23:26:30 +0530

    W1452232fdfdjjs

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -107,6 +107,264 @@ const Header = () => {
             <li>
               <Link to="/login">Login</Link>
             </li>
+          </ul>
+   
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+        
+        {/* Header Actions */}
+        <div className="header-actions">
+          <button className="icon-button">
+            <User size={20} />
+          </button>
+          <button className="icon-button">
+            <ShoppingBag size={20} />
+          </button>
+          <button className="cta-button">SHOP NOW</button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
+import { ShoppingBag, User } from "lucide-react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="main-header">
+        {/* Logo */}
+        <div className="logo">
+          <h1>
+            MIT BEAUTY<span>•</span>
+          </h1>
+          <p className="tagline">MIT COSMETICS</p>
+        </div>
+
+        {/* Navigation */}
+        <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav> {/* Navigation */}
+        <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav> {/* Navigation */}
+        <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>  <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>  <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>  <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>  <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>  <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>  <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>  <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>  <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+   
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+        
+        {/* Header Actions */}
+        <div className="header-actions">
+          <button className="icon-button">
+            <User size={20} />
+          </button>
+          <button className="icon-button">
+            <ShoppingBag size={20} />
+          </button>
+          <button className="cta-button">SHOP NOW</button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
+import { ShoppingBag, User } from "lucide-react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="main-header">
+        {/* Logo */}
+        <div className="logo">
+          <h1>
+            MIT BEAUTY<span>•</span>
+          </h1>
+          <p className="tagline">MIT COSMETICS</p>
+        </div>
+
+        {/* Navigation */}
+        <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav> {/* Navigation */}
+        <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav> {/* Navigation */}
+        <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>  <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>  <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>  <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>  <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>  <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>  <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
           </ul>  <nav>
           <ul className="nav-links">
             <li>
