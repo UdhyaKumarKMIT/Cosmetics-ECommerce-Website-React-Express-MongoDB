488001b2501a71f7d5264a01ae3a680be2cb3e87
UdhyaKumarKMIT <udhyak2004@gmail.com>
2025-08-13 23:35:52 +0530

    W14522ssdfds

--- src/components/promotions/SaleBanner.jsx
+++ src/components/promotions/SaleBanner.jsx
@@ -1,92 +1,120 @@
-sort(prices.begin(), prices.end());
-int n = prices.size();
-int min_cost = 0, max_cost = 0;
-int i = 0, j = n - 1;
-
-// Calculate minimum cost
-while (i <= j) {
-    min_cost += prices[i];
-    j -= k;
-    i++;
-}
-
-i = 0;
-j = n - 1;
-
-// Calculate maximum cost
-while (i <= j) {
-    max_cost += prices[j];
-    i += k;
-    j--;
-}
-
-return {min_cost, max_cost};
-sort(prices.begin(), prices.end());
-int n = prices.size();
-int min_cost = 0, max_cost = 0;
-int i = 0, j = n - 1;
-
-// Calculate minimum cost
-while (i <= j) {
-    min_cost += prices[i];
-    j -= k;
-    i++;
-}
-
-i = 0;
-j = n - 1;
-
-// Calculate maximum cost
-while (i <= j) {
-    max_cost += prices[j];
-    i += k;
-    j--;
-}
-
-return {min_cost, max_cost};
-sort(prices.begin(), prices.end());
-int n = prices.size();
-int min_cost = 0, max_cost = 0;
-int i = 0, j = n - 1;
-
-// Calculate minimum cost
-while (i <= j) {
-    min_cost += prices[i];
-    j -= k;
-    i++;
-}
-
-i = 0;
-j = n - 1;
-
-// Calculate maximum cost
-while (i <= j) {
-    max_cost += prices[j];
-    i += k;
-    j--;
-}
-
-return {min_cost, max_cost};
-sort(prices.begin(), prices.end());
-int n = prices.size();
-int min_cost = 0, max_cost = 0;
-int i = 0, j = n - 1;
-
-// Calculate minimum cost
-while (i <= j) {
-    min_cost += prices[i];
-    j -= k;
-    i++;
-}
-
-i = 0;
-j = n - 1;
-
-// Calculate maximum cost
-while (i <= j) {
-    max_cost += prices[j];
-    i += k;
-    j--;
-}
-
-return {min_cost, max_cost};
+class Solution {
+    public:
+        int maxProfit(vector<int>& prices) {
+            int buy=prices[0];
+            int maxProfit=0;
+            for(int i=1;i<prices.size();i++)
+            {
+                if(prices[i] < buy){
+                    buy=prices[i];
+                }else if(prices[i]-buy > maxProfit)
+                {
+                    maxProfit=prices[i]-buy;
+                }
+            }
+            return maxProfit;
+        }
+    };
+    class Solution {
+        public:
+            int maxProfit(vector<int>& prices) {
+                int buy=prices[0];
+                int maxProfit=0;
+                for(int i=1;i<prices.size();i++)
+                {
+                    if(prices[i] < buy){
+                        buy=prices[i];
+                    }else if(prices[i]-buy > maxProfit)
+                    {
+                        maxProfit=prices[i]-buy;
+                    }
+                }
+                return maxProfit;
+            }
+        };
+        class Solution {
+            public:
+                int maxProfit(vector<int>& prices) {
+                    int buy=prices[0];
+                    int maxProfit=0;
+                    for(int i=1;i<prices.size();i++)
+                    {
+                        if(prices[i] < buy){
+                            buy=prices[i];
+                        }else if(prices[i]-buy > maxProfit)
+                        {
+                            maxProfit=prices[i]-buy;
+                        }
+                    }
+                    return maxProfit;
+                }
+            };
+            class Solution {
+                public:
+                    int maxProfit(vector<int>& prices) {
+                        int buy=prices[0];
+                        int maxProfit=0;
+                        for(int i=1;i<prices.size();i++)
+                        {
+                            if(prices[i] < buy){
+                                buy=prices[i];
+                            }else if(prices[i]-buy > maxProfit)
+                            {
+                                maxProfit=prices[i]-buy;
+                            }
+                        }
+                        return maxProfit;
+                    }
+                };
+                class Solution {
+                    public:
+                        int maxProfit(vector<int>& prices) {
+                            int buy=prices[0];
+                            int maxProfit=0;
+                            for(int i=1;i<prices.size();i++)
+                            {
+                                if(prices[i] < buy){
+                                    buy=prices[i];
+                                }else if(prices[i]-buy > maxProfit)
+                                {
+                                    maxProfit=prices[i]-buy;
+                                }
+                            }
+                            return maxProfit;
+                        }
+                    };
+                    class Solution {
+                        public:
+                            int maxProfit(vector<int>& prices) {
+                                int buy=prices[0];
+                                int maxProfit=0;
+                                for(int i=1;i<prices.size();i++)
+                                {
+                                    if(prices[i] < buy){
+                                        buy=prices[i];
+                                    }else if(prices[i]-buy > maxProfit)
+                                    {
+                                        maxProfit=prices[i]-buy;
+                                    }
+                                }
+                                return maxProfit;
+                            }
+                        };
+                        class Solution {
+                            public:
+                                int maxProfit(vector<int>& prices) {
+                                    int buy=prices[0];
+                                    int maxProfit=0;
+                                    for(int i=1;i<prices.size();i++)
+                                    {
+                                        if(prices[i] < buy){
+                                            buy=prices[i];
+                                        }else if(prices[i]-buy > maxProfit)
+                                        {
+                                            maxProfit=prices[i]-buy;
+                                        }
+                                    }
+                                    return maxProfit;
+                                }
+                            };
+                                                                                                                
