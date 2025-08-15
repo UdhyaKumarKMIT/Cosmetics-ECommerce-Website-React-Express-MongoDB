198f0392861118b72e30f9c7ca9bfcc1aaf6a45e
UdhyaKumarKMIT <udhyak2004@gmail.com>
2025-08-15 23:11:55 +0530

    W14522ssfdsfdsfsfsddfds

--- src/components/promotions/SaleBanner.jsx
+++ src/components/promotions/SaleBanner.jsx
@@ -61,4 +61,256 @@ public:
         }
         return res;
     }
+};class Solution {
+    public:
+        string largestGoodInteger(string nums) {
+            string res="";
+            int len=nums.size();
+            int i=0;
+            while(i<len)
+            {
+                int j=i;
+                while(j<len && nums[i]==nums[j])
+                {
+                    j++;
+                }
+                if(j-i>=3)
+                {
+                    res=max(res,nums.substr(i,3));
+                }
+                i=j;
+            }
+            return res;
+        }
+    };class Solution {
+        public:
+            string largestGoodInteger(string nums) {
+                string res="";
+                int len=nums.size();
+                int i=0;
+                while(i<len)
+                {
+                    int j=i;
+                    while(j<len && nums[i]==nums[j])
+                    {
+                        j++;
+                    }
+                    if(j-i>=3)
+                    {
+                        res=max(res,nums.substr(i,3));
+                    }
+                    i=j;
+                }
+                return res;
+            }
+        };class Solution {
+public:
+    string largestGoodInteger(string nums) {
+        string res="";
+        int len=nums.size();
+        int i=0;
+        while(i<len)
+        {
+            int j=i;
+            while(j<len && nums[i]==nums[j])
+            {
+                j++;
+            }
+            if(j-i>=3)
+            {
+                res=max(res,nums.substr(i,3));
+            }
+            i=j;
+        }
+        return res;
+    }
+};class Solution {
+    public:
+        string largestGoodInteger(string nums) {
+            string res="";
+            int len=nums.size();
+            int i=0;
+            while(i<len)
+            {
+                int j=i;
+                while(j<len && nums[i]==nums[j])
+                {
+                    j++;
+                }
+                if(j-i>=3)
+                {
+                    res=max(res,nums.substr(i,3));
+                }
+                i=j;
+            }
+            return res;
+        }
+    };class Solution {
+        public:
+            string largestGoodInteger(string nums) {
+                string res="";
+                int len=nums.size();
+                int i=0;
+                while(i<len)
+                {
+                    int j=i;
+                    while(j<len && nums[i]==nums[j])
+                    {
+                        j++;
+                    }
+                    if(j-i>=3)
+                    {
+                        res=max(res,nums.substr(i,3));
+                    }
+                    i=j;
+                }
+                return res;
+            }
+        };class Solution {
+public:
+    string largestGoodInteger(string nums) {
+        string res="";
+        int len=nums.size();
+        int i=0;
+        while(i<len)
+        {
+            int j=i;
+            while(j<len && nums[i]==nums[j])
+            {
+                j++;
+            }
+            if(j-i>=3)
+            {
+                res=max(res,nums.substr(i,3));
+            }
+            i=j;
+        }
+        return res;
+    }
+};class Solution {
+    public:
+        string largestGoodInteger(string nums) {
+            string res="";
+            int len=nums.size();
+            int i=0;
+            while(i<len)
+            {
+                int j=i;
+                while(j<len && nums[i]==nums[j])
+                {
+                    j++;
+                }
+                if(j-i>=3)
+                {
+                    res=max(res,nums.substr(i,3));
+                }
+                i=j;
+            }
+            return res;
+        }
+    };class Solution {
+        public:
+            string largestGoodInteger(string nums) {
+                string res="";
+                int len=nums.size();
+                int i=0;
+                while(i<len)
+                {
+                    int j=i;
+                    while(j<len && nums[i]==nums[j])
+                    {
+                        j++;
+                    }
+                    if(j-i>=3)
+                    {
+                        res=max(res,nums.substr(i,3));
+                    }
+                    i=j;
+                }
+                return res;
+            }
+        };class Solution {
+public:
+    string largestGoodInteger(string nums) {
+        string res="";
+        int len=nums.size();
+        int i=0;
+        while(i<len)
+        {
+            int j=i;
+            while(j<len && nums[i]==nums[j])
+            {
+                j++;
+            }
+            if(j-i>=3)
+            {
+                res=max(res,nums.substr(i,3));
+            }
+            i=j;
+        }
+        return res;
+    }
+};class Solution {
+    public:
+        string largestGoodInteger(string nums) {
+            string res="";
+            int len=nums.size();
+            int i=0;
+            while(i<len)
+            {
+                int j=i;
+                while(j<len && nums[i]==nums[j])
+                {
+                    j++;
+                }
+                if(j-i>=3)
+                {
+                    res=max(res,nums.substr(i,3));
+                }
+                i=j;
+            }
+            return res;
+        }
+    };class Solution {
+        public:
+            string largestGoodInteger(string nums) {
+                string res="";
+                int len=nums.size();
+                int i=0;
+                while(i<len)
+                {
+                    int j=i;
+                    while(j<len && nums[i]==nums[j])
+                    {
+                        j++;
+                    }
+                    if(j-i>=3)
+                    {
+                        res=max(res,nums.substr(i,3));
+                    }
+                    i=j;
+                }
+                return res;
+            }
+        };class Solution {
+public:
+    string largestGoodInteger(string nums) {
+        string res="";
+        int len=nums.size();
+        int i=0;
+        while(i<len)
+        {
+            int j=i;
+            while(j<len && nums[i]==nums[j])
+            {
+                j++;
+            }
+            if(j-i>=3)
+            {
+                res=max(res,nums.substr(i,3));
+            }
+            i=j;
+        }
+        return res;
+    }
 };
