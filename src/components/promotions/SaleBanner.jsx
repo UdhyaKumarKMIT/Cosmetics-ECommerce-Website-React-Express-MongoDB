39f862b7dd1e42f303f06e0291773d99b0f32935
UdhyaKumarKMIT <udhyak2004@gmail.com>
2025-08-17 23:27:18 +0530

    W14522ssdfddfddssdss

--- src/components/promotions/SaleBanner.jsx
+++ src/components/promotions/SaleBanner.jsx
@@ -944,3 +944,20 @@ public:
         return res;
     }
 };
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<long long int>> printPascal(int n) 
+{
+    vector<vector<long long int>> res(n);
+
+    for (int i = 0; i < n; i++) {
+        res[i].resize(i + 1);   // each row has (i+1) elements
+        res[i][0] = res[i][i] = 1; // first and last element = 1
+
+        for (int j = 1; j < i; j++) {
+            res[i][j] = res[i-1][j-1] + res[i-1][j];
+        }
+    }
+    return res;
+}
