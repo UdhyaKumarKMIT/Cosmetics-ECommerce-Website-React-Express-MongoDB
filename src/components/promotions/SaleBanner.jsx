b38bdf7c6a31487060afebe22a5bc19bbab01d5c
UdhyaKumarKMIT <udhyak2004@gmail.com>
2025-08-19 23:45:07 +0530

    Wsfdsesdss

--- src/components/promotions/SaleBanner.jsx
+++ src/components/promotions/SaleBanner.jsx
@@ -1,8 +1,15 @@
-select Title, rating from earning left join IMDB on earning.movie_id = IMDB.movie_id where domestic > 100000000 and metacritic > 60 and IMDB.title like '%2012%'
-select Title, rating from earning left join IMDB on earning.movie_id = IMDB.movie_id where domestic > 100000000 and metacritic > 60 and IMDB.title like '%2012%'
-select Title, rating from earning left join IMDB on earning.movie_id = IMDB.movie_id where domestic > 100000000 and metacritic > 60 and IMDB.title like '%2012%'
-select Title, rating from earning left join IMDB on earning.movie_id = IMDB.movie_id where domestic > 100000000 and metacritic > 60 and IMDB.title like '%2012%'
-select Title, rating from earning left join IMDB on earning.movie_id = IMDB.movie_id where domestic > 100000000 and metacritic > 60 and IMDB.title like '%2012%'
-select Title, rating from earning left join IMDB on earning.movie_id = IMDB.movie_id where domestic > 100000000 and metacritic > 60 and IMDB.title like '%2012%'
-select Title, rating from earning left join IMDB on earning.movie_id = IMDB.movie_id where domestic > 100000000 and metacritic > 60 and IMDB.title like '%2012%'
-select Title, rating from earning left join IMDB on earning.movie_id = IMDB.movie_id where domestic > 100000000 and metacritic > 60 and IMDB.title like '%2012%'
+Farthest Smaller Right
+Difficulty: MediumAccuracy: 50.08%Submissions: 16K+Points: 4
+You are given an array arr[]. For each element at index i (0-based indexing), find the farthest index j to the right (i.e., j > i) such that arr[j] < arr[i]. If no such index exists for a given position, return -1 for that index. Return the resulting array of answers.Farthest Smaller Right
+Difficulty: MediumAccuracy: 50.08%Submissions: 16K+Points: 4
+You are given an array arr[]. For each element at index i (0-based indexing), find the farthest index j to the right (i.e., j > i) such that arr[j] < arr[i]. If no such index exists for a given position, return -1 for that index. Return the resulting array of answers.Farthest Smaller Right
+Difficulty: MediumAccuracy: 50.08%Submissions: 16K+Points: 4
+You are given an array arr[]. For each element at index i (0-based indexing), find the farthest index j to the right (i.e., j > i) such that arr[j] < arr[i]. If no such index exists for a given position, return -1 for that index. Return the resulting array of answers.Farthest Smaller Right
+Difficulty: MediumAccuracy: 50.08%Submissions: 16K+Points: 4
+You are given an array arr[]. For each element at index i (0-based indexing), find the farthest index j to the right (i.e., j > i) such that arr[j] < arr[i]. If no such index exists for a given position, return -1 for that index. Return the resulting array of answers.Farthest Smaller Right
+Difficulty: MediumAccuracy: 50.08%Submissions: 16K+Points: 4
+You are given an array arr[]. For each element at index i (0-based indexing), find the farthest index j to the right (i.e., j > i) such that arr[j] < arr[i]. If no such index exists for a given position, return -1 for that index. Return the resulting array of answers.Farthest Smaller Right
+Difficulty: MediumAccuracy: 50.08%Submissions: 16K+Points: 4
+You are given an array arr[]. For each element at index i (0-based indexing), find the farthest index j to the right (i.e., j > i) such that arr[j] < arr[i]. If no such index exists for a given position, return -1 for that index. Return the resulting array of answers.Farthest Smaller Right
+Difficulty: MediumAccuracy: 50.08%Submissions: 16K+Points: 4
+You are given an array arr[]. For each element at index i (0-based indexing), find the farthest index j to the right (i.e., j > i) such that arr[j] < arr[i]. If no such index exists for a given position, return -1 for that index. Return the resulting array of answers.
