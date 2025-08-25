bb0930f2678f4ef9ae3aeaa25209ac75d3cc3b98
UdhyaKumarKMIT <udhyak2004@gmail.com>
2025-08-25 21:59:06 +0530

    Wsfsdfssere2k

--- src/components/promotions/SaleBanner.jsx
+++ src/components/promotions/SaleBanner.jsx
@@ -1,118 +1,51 @@
 class Solution {
     public:
-      int minDaysBloom(vector<int>& arr, int m, int k) {
-          long long total = 1LL * m * k;
-          if (total > arr.size()) return -1;  // not enough flowers
-  
-          int left = *min_element(arr.begin(), arr.end());
-          int right = *max_element(arr.begin(), arr.end());
-          int ans = -1;
-  
-          while (left <= right) {
-              int mid = left + (right - left) / 2;
-              if (canMake(arr, mid, m, k)) {
-                  ans = mid;
-                  right = mid - 1;  // try earlier
-              } else {
-                  left = mid + 1;   // need more days
-              }
-          }
-          return ans;
-      }
-  
-    private:
-      bool canMake(vector<int>& arr, int day, int m, int k) {
-          int bouquets = 0, flowers = 0;
-          for (int x : arr) {
-              if (x <= day) {
-                  flowers++;
-                  if (flowers == k) {
-                      bouquets++;
-                      flowers = 0;
-                  }
-              } else {
-                  flowers = 0;
-              }
-          }
-          return bouquets >= m;
-      }
-  };
-  class Solution {
-    public:
-      int minDaysBloom(vector<int>& arr, int m, int k) {
-          long long total = 1LL * m * k;
-          if (total > arr.size()) return -1;  // not enough flowers
-  
-          int left = *min_element(arr.begin(), arr.end());
-          int right = *max_element(arr.begin(), arr.end());
-          int ans = -1;
-  
-          while (left <= right) {
-              int mid = left + (right - left) / 2;
-              if (canMake(arr, mid, m, k)) {
-                  ans = mid;
-                  right = mid - 1;  // try earlier
-              } else {
-                  left = mid + 1;   // need more days
-              }
-          }
-          return ans;
-      }
-  
-    private:
-      bool canMake(vector<int>& arr, int day, int m, int k) {
-          int bouquets = 0, flowers = 0;
-          for (int x : arr) {
-              if (x <= day) {
-                  flowers++;
-                  if (flowers == k) {
-                      bouquets++;
-                      flowers = 0;
-                  }
-              } else {
-                  flowers = 0;
-              }
-          }
-          return bouquets >= m;
-      }
-  };
-class Solution {
-  public:
-    int minDaysBloom(vector<int>& arr, int m, int k) {
-        long long total = 1LL * m * k;
-        if (total > arr.size()) return -1;  // not enough flowers
-
-        int left = *min_element(arr.begin(), arr.end());
-        int right = *max_element(arr.begin(), arr.end());
-        int ans = -1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (canMake(arr, mid, m, k)) {
-                ans = mid;
-                right = mid - 1;  // try earlier
-            } else {
-                left = mid + 1;   // need more days
-            }
-        }
-        return ans;
-    }
-
-  private:
-    bool canMake(vector<int>& arr, int day, int m, int k) {
-        int bouquets = 0, flowers = 0;
-        for (int x : arr) {
-            if (x <= day) {
-                flowers++;
-                if (flowers == k) {
-                    bouquets++;
-                    flowers = 0;
+        vector<int> findDiagonalOrder(vector<vector<int>>& matrix) {
+            if (matrix.empty() || matrix[0].empty()) return {};
+    
+            int m = matrix.size(), n = matrix[0].size();
+            vector<int> result(m * n);
+            int row = 0, col = 0;
+    
+            for (int i = 0; i < m * n; i++) {
+                result[i] = matrix[row][col];
+    
+                if ((row + col) % 2 == 0) {
+                    if (col == n - 1) row++;
+                    else if (row == 0) col++;
+                    else { row--; col++; }
+                } else {
+                    if (row == m - 1) col++;
+                    else if (col == 0) row++;
+                    else { row++; col--; }
                 }
+            }
+    
+            return result;
+        }
+    };class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& matrix) {
+        if (matrix.empty() || matrix[0].empty()) return {};
+
+        int m = matrix.size(), n = matrix[0].size();
+        vector<int> result(m * n);
+        int row = 0, col = 0;
+
+        for (int i = 0; i < m * n; i++) {
+            result[i] = matrix[row][col];
+
+            if ((row + col) % 2 == 0) {
+                if (col == n - 1) row++;
+                else if (row == 0) col++;
+                else { row--; col++; }
             } else {
-                flowers = 0;
+                if (row == m - 1) col++;
+                else if (col == 0) row++;
+                else { row++; col--; }
             }
         }
-        return bouquets >= m;
+
+        return result;
     }
 };
-    
