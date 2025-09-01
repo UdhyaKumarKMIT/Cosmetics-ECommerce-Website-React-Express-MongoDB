d0f06c7f18761ebbb5ce77bc8774edc18f3c9940
UdhyaKumarKMIT <udhyak2004@gmail.com>
2025-09-01 23:55:22 +0530

    First Push

--- src/components/promotions/SaleBanner.jsx
+++ src/components/promotions/SaleBanner.jsx
@@ -51,7 +51,63 @@ class Solution {
         
         
         
-        
+        class Solution {
+            public:
+                int maxWater(vector<int> &arr) {
+                    int n = arr.size();
+                    int left = 0, right = n - 1;
+                    int maxArea = 0;
+            
+                    while (left < right) {
+                        int height = min(arr[left], arr[right]);
+                        int width = right - left;
+                        maxArea = max(maxArea, height * width);
+            
+                        // Move the smaller line inward
+                        if (arr[left] < arr[right])
+                            left++;
+                        else
+                            right--;
+                    }
+            
+                    return maxArea;
+                }
+            };
+            
+            
+            
+            
+            
+            
+            class Solution {
+                public:
+                    int maxWater(vector<int> &arr) {
+                        int n = arr.size();
+                        int left = 0, right = n - 1;
+                        int maxArea = 0;
+                
+                        while (left < right) {
+                            int height = min(arr[left], arr[right]);
+                            int width = right - left;
+                            maxArea = max(maxArea, height * width);
+                
+                            // Move the smaller line inward
+                            if (arr[left] < arr[right])
+                                left++;
+                            else
+                                right--;
+                        }
+                
+                        return maxArea;
+                    }
+                };
+                
+                
+                
+                
+                
+                
+                
         
         
         
