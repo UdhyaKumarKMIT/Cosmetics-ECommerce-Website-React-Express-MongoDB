525b88b52670203b4dc8679e912015ed53d64384
UdhyaKumarKMIT <udhyak2004@gmail.com>
2025-09-11 22:16:47 +0530

    First Push1233333494kk4llss

--- src/components/promotions/SaleBanner.jsx
+++ src/components/promotions/SaleBanner.jsx
@@ -220,10 +220,128 @@ class Solution {
                                                                                         //    cout<<i<<"->"<<expSum4[i]<<", ";
                                                                                         }
                                                                                         long long op=0;
-                                                                                        for(auto& q: queries){
-                                                                                            int l=q[0]-1, r=q[1];
-                                                                                            op+=(expSum(r)-expSum(l)+1)/2;// ceiling of (expSum(r)-expSum(l))/2
-                                                                                        }
-                                                                                        return op;
-                                                                                    }
-                                                                               
+                                                                           public class Solution {
+    public String sortVowels(String s) {
+        int[] vowelCount = new int[11];
+        int[] countIndexMap = new int[128];
+        char[] result = s.toCharArray();
+        char[] charMap = "AEIOUaeiou".toCharArray();
+
+        for (int i = 0; i < charMap.length; i++) 
+            countIndexMap[charMap[i]] = i + 1;
+
+        for (char c : result) 
+            vowelCount[countIndexMap[c]]++;
+        int j = 1;
+        int i = 0;
+
+        while (j < vowelCount.length) {
+            if (vowelCount[j] > 0)
+                while (i < result.length) {
+                    if (countIndexMap[result[i]] == 0) {
+                        i++;
+                        continue;
+                    }
+                    vowelCount[j]--;
+                    result[i++] = charMap[j - 1];
+                    break;
+                }
+            else
+                j++;  
+        }
+        return new String(result);
+    }
+}public class Solution {
+    public String sortVowels(String s) {
+        int[] vowelCount = new int[11];
+        int[] countIndexMap = new int[128];
+        char[] result = s.toCharArray();
+        char[] charMap = "AEIOUaeiou".toCharArray();
+
+        for (int i = 0; i < charMap.length; i++) 
+            countIndexMap[charMap[i]] = i + 1;
+
+        for (char c : result) 
+            vowelCount[countIndexMap[c]]++;
+        int j = 1;
+        int i = 0;
+
+        while (j < vowelCount.length) {
+            if (vowelCount[j] > 0)
+                while (i < result.length) {
+                    if (countIndexMap[result[i]] == 0) {
+                        i++;
+                        continue;
+                    }
+                    vowelCount[j]--;
+                    result[i++] = charMap[j - 1];
+                    break;
+                }
+            else
+                j++;  
+        }
+        return new String(result);
+    }
+}public class Solution {
+    public String sortVowels(String s) {
+        int[] vowelCount = new int[11];
+        int[] countIndexMap = new int[128];
+        char[] result = s.toCharArray();
+        char[] charMap = "AEIOUaeiou".toCharArray();
+
+        for (int i = 0; i < charMap.length; i++) 
+            countIndexMap[charMap[i]] = i + 1;
+
+        for (char c : result) 
+            vowelCount[countIndexMap[c]]++;
+        int j = 1;
+        int i = 0;
+
+        while (j < vowelCount.length) {
+            if (vowelCount[j] > 0)
+                while (i < result.length) {
+                    if (countIndexMap[result[i]] == 0) {
+                        i++;
+                        continue;
+                    }
+                    vowelCount[j]--;
+                    result[i++] = charMap[j - 1];
+                    break;
+                }
+            else
+                j++;  
+        }
+        return new String(result);
+    }
+}public class Solution {
+    public String sortVowels(String s) {
+        int[] vowelCount = new int[11];
+        int[] countIndexMap = new int[128];
+        char[] result = s.toCharArray();
+        char[] charMap = "AEIOUaeiou".toCharArray();
+
+        for (int i = 0; i < charMap.length; i++) 
+            countIndexMap[charMap[i]] = i + 1;
+
+        for (char c : result) 
+            vowelCount[countIndexMap[c]]++;
+        int j = 1;
+        int i = 0;
+
+        while (j < vowelCount.length) {
+            if (vowelCount[j] > 0)
+                while (i < result.length) {
+                    if (countIndexMap[result[i]] == 0) {
+                        i++;
+                        continue;
+                    }
+                    vowelCount[j]--;
+                    result[i++] = charMap[j - 1];
+                    break;
+                }
+            else
+                j++;  
+        }
+        return new String(result);
+    }
+}
