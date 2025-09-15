d35ddbb8db3e776d710056194c2a4986a075795b
UdhyaKumarKMIT <udhyak2004@gmail.com>
2025-09-15 22:14:19 +0530

    First Push1233333494kk4llsslllsD

--- src/components/promotions/SaleBanner.jsx
+++ src/components/promotions/SaleBanner.jsx
@@ -510,5 +510,37 @@ private:
     bool isVowel(char c) {
         c = tolower(c);
         return c=='a'||c=='e'||c=='i'||c=='o'||c=='u';
+    }class Solution {
+public:
+    int canBeTypedWords(string text, string broken){
+        unordered_set<char> brokenKeys;
+        int count = 0;
+
+        for (auto& c : broken){
+            brokenKeys.insert(c);
+        }
+
+        stringstream ss(text);
+        string word;
+        vector<string> words;
+
+        while (ss >> word){
+            words.push_back(word);
+        }
+
+        for (auto w : words)
+        {
+            for (char c : w)
+            {
+                if (brokenKeys.find(c) != brokenKeys.end())
+                {
+                    count++;
+                    break;
+                }
+            }
+        }
+
+        return words.size() - count;
     }
+};
 };
