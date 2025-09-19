95e2739d0c4d74029f1fd6bbec3407f7d0ceecba
UdhyaKumarKMIT <udhyak2004@gmail.com>
2025-09-19 23:47:41 +0530

    Bus Search

--- src/components/promotions/SaleBanner.jsx
+++ src/components/promotions/SaleBanner.jsx
@@ -714,8 +714,62 @@ public:
                                 cuisineNameToSortedInfo[toUpdate.cuisine].insert(toUpdate);
                             }
                         
-                            string highestRated(const string& cuisine) const {
-                                return cuisineNameToSortedInfo.at(cuisine).begin()->name;
-                            }
-                        };
+                      class Spreadsheet {
+public:
+    int rows;
+    unordered_map<string, int> cells;
+
+    Spreadsheet(int rows) {
+        this->rows = rows;
+    }
+    
+    void setCell(string cell, int value) {
+        cells[cell] = value;
+    }
+    
+    void resetCell(string cell) {
+        cells.erase(cell);
+    }
+    
+    int getValue(string formula) {
+        formula = formula.substr(1);
+        int plus = formula.find('+');
+        string a = formula.substr(0, plus);
+        string b = formula.substr(plus + 1);
+
+        auto val = [&](string s) {
+            return all_of(s.begin(), s.end(), ::isdigit) ? stoi(s) : cells[s];
+        };
+
+        return val(a) + val(b);
+    }
+};class Spreadsheet {
+public:
+    int rows;
+    unordered_map<string, int> cells;
+
+    Spreadsheet(int rows) {
+        this->rows = rows;
+    }
+    
+    void setCell(string cell, int value) {
+        cells[cell] = value;
+    }
+    
+    void resetCell(string cell) {
+        cells.erase(cell);
+    }
+    
+    int getValue(string formula) {
+        formula = formula.substr(1);
+        int plus = formula.find('+');
+        string a = formula.substr(0, plus);
+        string b = formula.substr(plus + 1);
+
+        auto val = [&](string s) {
+            return all_of(s.begin(), s.end(), ::isdigit) ? stoi(s) : cells[s];
+        };
+
+        return val(a) + val(b);
+    }
 };
