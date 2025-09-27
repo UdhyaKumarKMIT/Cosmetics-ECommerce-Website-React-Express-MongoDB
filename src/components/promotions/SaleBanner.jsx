15835d525a116de25f87b7a8ebf02888ec1f86d4
UdhyaKumarKMIT <udhyak2004@gmail.com>
2025-09-27 23:33:32 +0530

    Bus lll

--- src/components/promotions/SaleBanner.jsx
+++ src/components/promotions/SaleBanner.jsx
@@ -120,28 +120,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import React from 'react';-types';
 import React from 'rclass Solution {
-    public:
-        int triangleNumber(vector<int>& nums) {
-            sort(nums.begin(),nums.end());
-            int res=0,n=nums.size(),l=0,h=0;
-    //condition a+b>c so we are fixing c and trying to find nums[a]+nums[b]>nums[c]
-            for(int i=n-1;i>=2;i--)
-            {
-                l=0;
-                h=i-1;
-                while(l<h)
-                {
-                    if(nums[l]+nums[h]>nums[i])
-                    {
-                        res+=(h-l);
-                        h--;
-                    }else
-                    {
-                        l++;
-                    }
-                }
-            }
-            return res;
-        }
-    };
-    };
+    import PropTypes from 'prop-types';
+    import React from 'react';-types';
+    import React from 'rclass Solution {
+        import PropTypes from 'prop-types';
+        import React from 'react';-types';
+        import React from 'rclass Solution {
+            import PropTypes from 'prop-types';
+            import React from 'react';-types';
+            import React from 'rclass Solution {
+                import PropTypes from 'prop-types';
+                import React from 'react';-types';
+                import React from 'rclass Solution {
+                    import PropTypes from 'prop-types';
+                    import React from 'react';-types';
+                    import React from 'rclass Solution {
