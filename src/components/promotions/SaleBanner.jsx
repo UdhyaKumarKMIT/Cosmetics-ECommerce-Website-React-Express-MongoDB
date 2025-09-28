d574067021fb4efa6928c933a2af74e0751cf5b6
UdhyaKumarKMIT <udhyak2004@gmail.com>
2025-09-28 23:36:28 +0530

    Bus llld

--- src/components/promotions/SaleBanner.jsx
+++ src/components/promotions/SaleBanner.jsx
@@ -127,7 +127,23 @@ import React from 'rclass Solution {
         import React from 'react';-types';
         import React from 'rclass Solution {
             import PropTypes from 'prop-types';
-            import React from 'react';-types';
+            import React sort(A.begin(), A.end());
+        for (int i = A.size() - 1 ; i > 1; --i)
+            if (A[i] < A[i - 1] + A[i - 2])
+                return A[i] + A[i - 1] + A[i - 2];
+        return 0;sort(A.begin(), A.end());
+        for (int i = A.size() - 1 ; i > 1; --i)
+            if (A[i] < A[i - 1] + A[i - 2])
+                return A[i] + A[i - 1] + A[i - 2];
+        return 0;sort(A.begin(), A.end());
+        for (int i = A.size() - 1 ; i > 1; --i)
+            if (A[i] < A[i - 1] + A[i - 2])
+                return A[i] + A[i - 1] + A[i - 2];
+        return 0;sort(A.begin(), A.end());
+        for (int i = A.size() - 1 ; i > 1; --i)
+            if (A[i] < A[i - 1] + A[i - 2])
+                return A[i] + A[i - 1] + A[i - 2];
+        return 0;from 'react';-types';
             import React from 'rclass Solution {
                 import PropTypes from 'prop-types';
                 import React from 'react';-types';
