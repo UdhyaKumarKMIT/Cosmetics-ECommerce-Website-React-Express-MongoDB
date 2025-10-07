6cc84b40b36e0d49662917890a7edb4da9755c1f
Udhya Kumar K <166237922+UdhyaKumarKMIT@users.noreply.github.com>
2025-10-07 23:17:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,62 @@ int n ;
 int vis[51][51];
 int dx[4] = {1,-1,0,0} ;
 int dy[4] = {0,0,1,-1} ;
-bool is_valid(int i , int j,auto& arr,int mid)
+bool is_valid(int i , intclass Solution {
+public:
+int n ;
+int vis[51][51];
+int dx[4] = {1,-1,0,0} ;
+int dy[4] = {0,0,1,-1} ;
+bool is_valid(int i , intclass UnionFind {
+public:
+    vector<int> parent;
+
+    UnionFind(int size) : parent(size + 1) {
+        iota(parent.begin(), parent.end(), 0);
+    }
+
+    int find(int i) {
+        if (i == parent[i])
+            return i;
+        parent[i] = find(parent[i]);
+        return parent[i];
+    }
+
+    void unite(int i) { parent[i] = find(i + 1); }
+};
+
+class Solution {
+public:
+    vector<int> avoidFlood(vector<int>& rain) {
+        int n = rain.size();
+        UnionFind uf(n + 1);
+        unordered_map<int, int> map;
+        vector<int> res(n, 1);
+
+        for (int i = 0; i < n; i++) {
+            int lake = rain[i];
+            if (lake == 0) continue;
+
+            res[i] = -1;
+            uf.unite(i);
+
+            if (map.find(lake) != map.end()) {
+                int prev = map[lake];
+                int dry = uf.find(prev + 1);
+
+                if (dry >= i) return {};
+
+                res[dry] = lake;
+                uf.unite(dry);
+                map[lake] = i;
+            } else {
+                map[lake] = i;
+            }
+        }
+
+        return res;
+    }
+}; j,auto& arr,int mid)
 {
     return i>=0 and j>=0 and i<n and j<n and arr[i][j]<=mid and !vis[i][j] ;
 }
