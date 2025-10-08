fbe2e150ec00d38c8729fe898ee3f13326a24aa8
Udhya Kumar K <166237922+UdhyaKumarKMIT@users.noreply.github.com>
2025-10-08 21:57:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,21 @@ function App() {
   return (
     class Solution {
 public:
-int n ;
+int n ;class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        int n = potions.size(), m = spells.size();
+        vector<int> ret(m);
+        sort(potions.begin(), potions.end());
+        for (int r = 0; r < m; ++r) {
+            long long s = spells[r];
+            long long temp = (success + s - 1) / s;
+            auto it = lower_bound(potions.begin(), potions.end(), temp);
+            ret[r] = n - static_cast<int>(it - potions.begin());
+        }
+        return ret;
+    }
+};
 int vis[51][51];
 int dx[4] = {1,-1,0,0} ;
 int dy[4] = {0,0,1,-1} ;
