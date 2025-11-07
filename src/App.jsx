68a8a1c34e4d782e9667528a6f6d7105e4f6a5d6
Udhya Kumar K <166237922+UdhyaKumarKMIT@users.noreply.github.com>
2025-11-07 23:19:19 +0530

    Add maxPower function for station power calculation

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,45 @@ function App() {Mama..
 Aadhar la address change pana father name remove agirucku 😭😭😭
 
 KYC complete aga matangiduQ18 of 25
-What are the benefits of Continuous Integration? [Multiple response question]
+What are the benefits of Continuous Integr
+  class Solution {
+ public:
+  long long maxPower(vector<int>& stations, int r, int k) {
+    long left = ranges::min(stations);
+    long right = accumulate(stations.begin(), stations.end(), 0L) + k + 1;
+    while (left < right) {
+      const long mid = (left + right) / 2;
+      if (check(stations, r, k, mid))
+        left = mid + 1;
+      else
+        right = mid;
+    }
+    return left - 1;
+  }
+
+ private:
+  bool check(vector<int> stations, int r, int additionalStations,
+             long minPower) {
+    const int n = stations.size();
+    long power = accumulate(stations.begin(), stations.begin() + r, 0L);
+
+    for (int i = 0; i < n; ++i) {
+      if (i + r < n)
+        power += stations[i + r];
+      if (power < minPower) {
+        const long requiredPower = minPower - power;
+        if (requiredPower > additionalStations)
+          return false;
+        stations[min(n - 1, i + r)] += requiredPower;
+        additionalStations -= requiredPower;
+        power += requiredPower;
+      }
+      if (i - r >= 0)
+        power -= stations[i - r];
+    }
+    return true;
+  }
+};ation? [Multiple response question]
 7 Automated Build integrates various tools
 Lesser deployment risk as smaller changes are deployed
 Prnnnhnnnnovides ability to completely rebuild and test applications
