1b703108bca04f7b9aeb80c92d371c59834c3186
Udhya Kumar K <166237922+UdhyaKumarKMIT@users.noreply.github.com>
2025-11-22 22:10:31 +0530

    Fix import statements and clean up code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import 
+import
+  
+  
+  
+  9150653370exactly use the same face 
+.little slime the person terror look. remove the word str and replace UK .red and blood bank with some person black masscitimport 
 import 
 import 
 
